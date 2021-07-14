f33d033d009a7f1374c0a6bb4b896bfa7d627160
Ayman Hossam <aymanhossamgad@gmail.com>
2021-07-14 02:26:54 +0200

    add searchbar

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, TextInput } from 'react-native';
 import colors from '../styles/colors';
 import { useBooks } from '../actions/hooks';
 import { BookCard } from '../components';
@@ -9,21 +9,50 @@ import {
 } from 'react-native-size-matters';
 
 const HomePage = () => {
-  const { status, data, error, isFetching } = useBooks({
-    bibkeys: 'ISBN:0451526538,ISBN:0201558025',
+  const [searchText, setSearchText] = useState('');
+  const [query, setQuery] = useState('');
+
+  const { data, isFetching } = useBooks({
+    bibkeys: query,
     jscmd: 'data',
   });
+
+  //   bibkeys: 'ISBN:0451526538,ISBN:0201558025',
+
+  const onChangeText = value => {
+    setSearchText(value.replace(/\D/g, ','));
+  };
+
+  const processQuery = () => {
+    const isbnArray = searchText.split(' ');
+    const queryValue = isbnArray.reduce(
+      (value, isbn) => (value += `ISBN:${isbn},`),
+      ''
+    );
+    setQuery(queryValue);
+  };
 
   const renderItem = ({ item }) => <BookCard book={item} />;
   const renderSeparator = () => <View style={styles.separator} />;
 
   return !isFetching ? (
     <View style={styles.container}>
+      <View style={styles.searchbar}>
+        <TextInput
+          value={searchText}
+          onChangeText={onChangeText}
+          keyboardType="numeric"
+          placeholder="Search.."
+          onSubmitEditing={processQuery}
+        />
+      </View>
       <FlatList
         data={Object.values(data)}
         keyExtractor={item => item.key}
         renderItem={renderItem}
         ItemSeparatorComponent={renderSeparator}
+        contentContainerStyle={styles.list}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   ) : (
@@ -37,10 +66,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
-    padding: moderateScale(20),
-    paddingHorizontal: moderateScale(10),
+    paddingTop: moderateScale(20),
+    paddingHorizontal: moderateScale(15),
   },
   separator: {
     height: moderateVerticalScale(10),
+  },
+  searchbar: {
+    height: moderateVerticalScale(40),
+    borderRadius: moderateScale(6),
+    borderWidth: 0.5,
+    justifyContent: 'center',
+    paddingHorizontal: moderateScale(10),
+    marginBottom: moderateScale(10),
+  },
+  list: {
+    paddingVertical: moderateVerticalScale(15),
   },
 });
